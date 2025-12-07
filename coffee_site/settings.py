e1bd7d64a990ab733479266009edcf0543c14dd8
Selinclb <selincelebi.dev@gmail.com>
2025-12-08 02:07:12 +0300

    new version 2026

--- coffee_site/settings.py
+++ coffee_site/settings.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-
+from django.core.management.utils import get_random_secret_key
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jlyksa201879mj88d@k=7t-0wrz&1_m5lk0)w60-yo)vdh4u^)'
+SECRET_KEY = os.environ.get("SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
