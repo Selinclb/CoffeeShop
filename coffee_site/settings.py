ace27dbf12b90d21adab3ffa319f60d7279d3983
Selinclb <selincelebi.dev@gmail.com>
2025-12-08 16:44:10 +0300

    requirements.txt

--- coffee_site/settings.py
+++ coffee_site/settings.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -80,7 +79,7 @@ UNFOLD = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
